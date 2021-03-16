f6d1381e37340824523c8cb954a32d272ec6d175
hiroki <takahaashi1223@gmail.com>
2021-03-16 14:02:07 +0900

    Extract css & html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode:'development',
@@ -34,13 +35,16 @@ module.exports = {
                                 name : '[name].[ext]',
                                 outputPath: 'images',
                                 pubulicPath: 'images'
-                            }
-                        }
-                ]
-            }
-        ]
+                         },
+                     },
+                ],
+            },
+        ],
     },
     plugins: [
         new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
     ],
 }
